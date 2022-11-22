e564a02837e0c2f469a6c1cd333d2c38a2e344a4
Mandy Chen <ruxchen@microsoft.com>
2022-11-22 13:00:19 -0800

    Update SW location at registration

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const registerServiceWorker = async () => {
   try {
-    await navigator.serviceWorker.register('/sw.js', { scope: '/widgets-pwa/' });
+    await navigator.serviceWorker.register('/widgets-pwa/sw.js', { scope: '/widgets-pwa/' });
     console.log('Service worker registered');
   } catch (e) {
     console.log(`Registration failed: ${e}`);
